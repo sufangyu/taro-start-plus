59f7573a877248f34cf5e113f5da1f908b285d24
sufangyu <304683191@qq.com>
2023-06-29 11:23:53 +0800

    chore: 支持飞书小程序

--- config/index.js
+++ config/index.js
@@ -12,7 +12,9 @@ const config = {
   },
   sourceRoot: 'src',
   outputRoot: 'dist',
-  plugins: [],
+  plugins: [
+    '@tarojs/plugin-platform-lark'
+  ],
   alias: {
     '@': path.resolve(__dirname, '..', 'src'),
   },
