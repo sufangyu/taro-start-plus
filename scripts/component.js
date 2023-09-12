3aa4c57e01c689d4c5cb393807aa9480e32ee8e6
sufangyu <304683191@qq.com>
2023-09-12 17:31:52 +0800

    chore: 调整 组件初始化模版内容

--- scripts/component.js
+++ scripts/component.js
@@ -9,27 +9,31 @@ const { mkdirsSync } = require('./utils');
 
 const dirName = process.argv[2];
 
+console.log(dirName);
+
 if (!dirName) {
   console.warn('组件名称不能为空');
   console.log('eg: npm run tem test');
   process.exit(0);
 }
 
-const dirname = `./src/common/components/${dirName}`;
-const isExist = fs.existsSync(dirname);
+const fullpath = `./src/common/components/${dirName}`;
+const isExist = fs.existsSync(fullpath);
 if (isExist) {
-  console.warn(`组件(${dirName})已存在`);
+  console.warn(`组件(${fullpath})已存在`);
   process.exit(0);
 }
 
 
 // 模板
 const indexTep = `import { View } from '@tarojs/components';
+
+import { Props } from './types';
 import './index.scss';
 
-interface Props {}
-
 const Index = (props: Props) => {
+  const {} = props;
+  
   return (
     <View className="container">
       This is empty component
@@ -40,14 +44,23 @@ const Index = (props: Props) => {
 export default Index;
 `;
 
+// types 模板
+const typesTep = `export interface Props {}
+`;
+
 // scss 模板
 const scssTep = ``;
 
+// 入口
+const entryTep = `export { default as ${dirName} } from './${dirName}';`;
 
-mkdirsSync(dirname); // mkdir $1
-process.chdir(dirname); // cd $1
-fs.writeFileSync(`index.tsx`, indexTep); // write tsx
+
+mkdirsSync(fullpath); // mkdir $1
+process.chdir(fullpath); // cd $1
+fs.writeFileSync(`${dirName}.tsx`, indexTep); // write tsx
+fs.writeFileSync(`types.ts`, typesTep); // write types
 fs.writeFileSync(`index.scss`, scssTep); // write scss
+fs.writeFileSync(`index.ts`, entryTep); // write tsx
 
 console.log('创建成功');
 process.exit(0);
